51ab3ffc2de8a611019e46abb89bd2860208f7ea
Christian Farinella <christian.farinella@ai.astra.co.id>
2023-03-19 11:39:31 +1100

    feat: updates to libs

--- src/code/PlatformX.Startup/Extensions/ConfigurationExtensions.cs
+++ src/code/PlatformX.Startup/Extensions/ConfigurationExtensions.cs
@@ -9,31 +9,27 @@ namespace PlatformX.Startup.Extensions
         {
             var bootstrapConfiguration = new BootstrapConfiguration
             {
-                EnvironmentName = Environment.GetEnvironmentVariable("EnvironmentName"),
-                AspNetCoreEnvironmentName = Environment.GetEnvironmentVariable("AspNetCoreEnvironmentName"),
-                Prefix = Environment.GetEnvironmentVariable("Prefix"),
-                RoleKey = Environment.GetEnvironmentVariable("RoleKey"),
-                PortalName = Environment.GetEnvironmentVariable("PortalName"),
-                ServiceName = Environment.GetEnvironmentVariable("ServiceName"),
-                Layer = Environment.GetEnvironmentVariable("Layer"),
-                AzureRegion = Environment.GetEnvironmentVariable("AzureRegion"),
-                AzureLocation = Environment.GetEnvironmentVariable("AzureLocation"),
-                AzureTenantId = Environment.GetEnvironmentVariable("AzureTenantId"),
-                AwsRegion = Environment.GetEnvironmentVariable("AwsRegion"),
-                SsmPrefx = Environment.GetEnvironmentVariable("SsmPrefx"),
-                LogMessages = Environment.GetEnvironmentVariable("LogMessages") == "Y",
-                LogWarnings = Environment.GetEnvironmentVariable("LogWarnings") == "Y",
-                LogErrors = Environment.GetEnvironmentVariable("LogErrors") == "Y",
-                DBName = Environment.GetEnvironmentVariable("DBName"),
-                PlatformServiceMetaData = Environment.GetEnvironmentVariable("PlatformServiceMetaData"),
-                ServiceKeys = Environment.GetEnvironmentVariable("ServiceKeys"),
-                RuntimeConfiguration = Environment.GetEnvironmentVariable("RuntimeConfiguration"),
-                ServiceSymmetricKeyName = Environment.GetEnvironmentVariable("ServiceSymmetricKeyName"),
-                ServerName = Environment.GetEnvironmentVariable("ServerName"),
-                BuildNumber = Environment.GetEnvironmentVariable("BuildNumber"),
-                ContainerType = Environment.GetEnvironmentVariable("ContainerType"),
-                CorsOrigins = Environment.GetEnvironmentVariable("CorsOrigins"),
+                EnvironmentName = Environment.GetEnvironmentVariable("ENVIRONMENT_NAME"),
+                AspNetCoreEnvironmentName = Environment.GetEnvironmentVariable("ASPNET_CORE_ENVIRONMENT_NAME"),
+                Prefix = Environment.GetEnvironmentVariable("PREFIX"),
+                RoleKey = Environment.GetEnvironmentVariable("ROLEKEY"),
+                PortalName = Environment.GetEnvironmentVariable("PORTAL_NAME"),
+                ServiceName = Environment.GetEnvironmentVariable("SERVICE_NAME"),
+                Layer = Environment.GetEnvironmentVariable("LAYER"),
+                AzureRegion = Environment.GetEnvironmentVariable("AZURE_REGION"),
+                AzureLocation = Environment.GetEnvironmentVariable("AZURE_LOCATION"),
+                AzureTenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID"),
+                AwsRegion = Environment.GetEnvironmentVariable("AWS_REGION"),
+                SsmPrefix = Environment.GetEnvironmentVariable("SSM_PREFIX"),
+                ServiceKeys = Environment.GetEnvironmentVariable("SERVICE_KEYS"),
+                RuntimeConfiguration = Environment.GetEnvironmentVariable("RUNTIME_CONFIGURATION"),
+                ServiceSymmetricKeyName = Environment.GetEnvironmentVariable("SERVICE_SYMMETRIC_KEY"),
+                HostEnvironmentType = Environment.GetEnvironmentVariable("HOST_ENVIRONMENT_TYPE"),
+                CorsOrigins = Environment.GetEnvironmentVariable("CORS_ORIGINS"),
             };
+
+            // Check environment name strings
+
 
             services.AddSingleton(bootstrapConfiguration);
 
